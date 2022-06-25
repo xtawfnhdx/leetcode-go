41790f4626348c2d5e20db0e6f6b1570280387be
xtawfnhdx <zhangweilong.001@163.com>
2022-06-25 15:21:06 +0800

    学习：第九章完成

---
+++ the_way_to_go/09/9.4.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"./pack1"
+	"fmt"
+)
+
+func main() {
+	st := pack1.ReturnStr()
+	fmt.Println(st)
+}
---
+++ the_way_to_go/09/pack1/pack1.go
@@ -0,0 +1,18 @@
+//Author：zwl
+//Create：2022-06-225
+//Msg：this is a test package
+//Name：pack1
+
+//test package
+package pack1
+
+//int param
+var Pack1Int = 42
+
+//float param
+var Pack1Float = 3.14
+
+//return test string
+func ReturnStr() string {
+	return "hello"
+}
